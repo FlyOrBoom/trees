b8a9f9f092c104806b1506469aa460f2081c3b1d
Xing <31298876+FlyOrBoom@users.noreply.github.com>
2020-12-01 17:54:07 -0800

    Create 00002.glsl

---
+++ 00002.glsl
@@ -0,0 +1,14 @@
+/* TREES
+ * Graphics project to recreate random frames
+ * of trees from this video to teach myself
+ * fractals n other stuff:
+ * https://www.youtube.com/watch?v=XkfmrXLxaNk
+ * t=2s
+ */
+
+/* TODO
+ * Sky
+ * Sticks
+ * Leaves
+ * Blur
+ */
